b9b1d7804dba64b9a7c705435b7b29b8c4b7c691
Alexander Pankratov <s.pankratoff@gmail.com>
2024-08-08 01:42:23 +0200

    Feat: add microsoft, google and openai domains

--- patches/fix.sh
+++ patches/fix.sh
@@ -3,3 +3,5 @@
 # fix invalid domains
 # https://ntc.party/t/129/636
 sed -i -E "s/(CHARSET=UTF-8 idn)/\1 --no-tld | grep -Fv 'xn--'/g" /root/antizapret/parse.sh
+
+sed -i "/\b\(googleusercontent\|cloudfront\|deviantart\)\b/d" /root/antizapret/config/exclude-regexp-dist.awk
