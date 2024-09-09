5af7bb99cf2e81695c5435eb0156cf26d4ebbad1
Dimariqe <dimariqe@gmail.com>
2024-09-09 18:08:05 +0700

    FORCE_FORWARD_DNS + extends wireguard amnezia
    EN:
    - (WG only) Added FORCE_FORWARD_DNS parameter for wireguard. Redirects udp traffic on port 53 to antizapret dns.
    - (WG only) The FORCE_FORWARD_DNS_PORTS parameter can be used to change port 53 to one or more, separated by a space: FORCE_FORWARD_DNS_PORTS="53 5353"
    - Wireguard Amnezia is now inherited from regular Wireguard.
    
    RU:
    (Только WG) Добавил параметр FORCE_FORWARD_DNS для wireguard. Перенаправляет трафик udp на 53 порту к antizapret dns.
    (Только WG) Параметром FORCE_FORWARD_DNS_PORTS можно изменить 53 порт на один или несколько, через пробел: FORCE_FORWARD_DNS_PORTS="53 5353"
    Wireguard Amnezia теперь наследуется от обычного Wireguard.

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -11,8 +11,12 @@ export WG_HOST=$(curl -4 icanhazip.com)
 export AZ_HOST=$(dig +short antizapret-vpn)
 
 ip route add 10.224.0.0/15 via $AZ_HOST
-iptables -t nat -A OUTPUT -d 10.225.255.254/32 -j DNAT --to-destination $AZ_HOST
-iptables -t nat -A PREROUTING -d 10.225.255.254/32 -j DNAT --to-destination $AZ_HOST
-iptables -t nat -A POSTROUTING -d 10.225.255.254/32 -j MASQUERADE
+
+if [[ ${FORCE_FORWARD_DNS:-false} == true ]]; then
+    dnsPorts=${FORCE_FORWARD_DNS_PORTS:-"53"}
+    for dnsPort in $dnsPorts; do
+        iptables -t nat -A PREROUTING -i wg0 -p udp -m udp --dport $dnsPort -j DNAT --to-destination $AZ_HOST
+    done
+fi
 
 exec /usr/bin/dumb-init node server.js
