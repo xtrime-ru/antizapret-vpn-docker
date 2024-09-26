302c04a0e81693ae6afac2a169e634e9ed24edf6
Alexander Pankratov <s.pankratoff@gmail.com>
2024-09-26 13:08:55 +0200

    Fix iperf

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -11,8 +11,10 @@ export WG_HOST=$(curl -4 icanhazip.com)
 export AZ_HOST=$(dig +short antizapret-vpn)
 
 ip route add 10.224.0.0/15 via $AZ_HOST
+iptables -t nat -A PREROUTING -d 10.224.0.1/32 -j DNAT --to-destination $AZ_HOST
+iptables -t nat -A OUTPUT -d 10.224.0.1/32 -j DNAT --to-destination $AZ_HOST
 
-if [[ ${FORCE_FORWARD_DNS:-false} == true ]]; then
+if [[ ${FORCE_FORWARD_DNS:-true} == true ]]; then
     dnsPorts=${FORCE_FORWARD_DNS_PORTS:-"53"}
     for dnsPort in $dnsPorts; do
         iptables -t nat -A PREROUTING -i wg0 -p udp -m udp --dport $dnsPort -j DNAT --to-destination $AZ_HOST
