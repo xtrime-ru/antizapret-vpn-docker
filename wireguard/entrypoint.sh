d136bc9a3b91b87d5c91a59c610212ddfd2be1ac
hondaspb <hondaspb@gmail.com>
2024-09-27 19:00:27 +0300

    Update ferm.conf for iperf to work

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -11,8 +11,6 @@ export WG_HOST=$(curl -4 icanhazip.com)
 export AZ_HOST=$(dig +short antizapret-vpn)
 
 ip route add 10.224.0.0/15 via $AZ_HOST
-iptables -t nat -A PREROUTING -d 10.224.0.1/32 -j DNAT --to-destination $AZ_HOST
-iptables -t nat -A OUTPUT -d 10.224.0.1/32 -j DNAT --to-destination $AZ_HOST
 
 if [[ ${FORCE_FORWARD_DNS:-true} == true ]]; then
     dnsPorts=${FORCE_FORWARD_DNS_PORTS:-"53"}
