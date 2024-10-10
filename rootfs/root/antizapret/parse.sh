f8fe6d934d2ac7830aa8990458a3e145e41567d7
Dimariqe <dimariqe@gmail.com>
2024-10-10 15:28:27 +0700

    fixed

--- rootfs/root/antizapret/parse.sh
+++ rootfs/root/antizapret/parse.sh
@@ -3,8 +3,7 @@ set -e
 
 HERE="$(dirname "$(readlink -f "${0}")")"
 cd "$HERE"
-
-source config/config.sh
+RESOLVE_NXDOMAIN="no"
 
 # Extract domains from list
 if [[ ! -n $SKIP_UPDATE_FROM_ZAPRET ]]; then
@@ -13,7 +12,7 @@ else
    echo -e "\n" > result/hostlist_original.txt
 fi
 
-for file in config/custom/{include,exclude}-hosts-custom.txt; do
+for file in config/custom/{include,exclude}-{hosts,ips}-custom.txt; do
     basename=$(basename $file | sed 's|-custom.txt||')
     sort -u $file config/${basename}-dist.txt > temp/${basename}.txt
 done
@@ -31,17 +30,8 @@ then
     awk -f scripts/getzones.awk temp/hostlist_original_with_include.txt | grep -v -F -x -f temp/exclude-hosts.txt | sort -u > result/hostlist_zones.txt
 fi
 
-# Generate a list of IP addresses
-# awk -F';' '$1 ~ /\// {print $1}' temp/list.csv | grep -P '([0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9]{1,2}' -o | sort -Vu > result/iplist_special_range.txt
-# awk -F ';' '($1 ~ /^([0-9]{1,3}\.){3}[0-9]{1,3}/) {gsub(/\|/, RS, $1); print $1}' temp/list.csv | \
-#     awk '/^([0-9]{1,3}\.){3}[0-9]{1,3}$/' | sort -u > result/iplist_all.txt
-# awk -F ';' '($1 ~ /^([0-9]{1,3}\.){3}[0-9]{1,3}/) && (($2 == "" && $3 == "") || ($1 == $2)) {gsub(/\|/, RS); print $1}' temp/list.csv | \
-#     awk '/^([0-9]{1,3}\.){3}[0-9]{1,3}$/' | sort -u > result/iplist_blockedbyip.txt
-# grep -F -v '33-4/2018' temp/list.csv | grep -F -v '33а-5536/2019' | \
-#     awk -F ';' '($1 ~ /^([0-9]{1,3}\.){3}[0-9]{1,3}/) && (($2 == "" && $3 == "") || ($1 == $2)) {gsub(/\|/, RS); print $1}' | \
-#     awk '/^([0-9]{1,3}\.){3}[0-9]{1,3}$/' | sort -u > result/iplist_blockedbyip_noid2971.txt
 awk -F ';' '$1 ~ /\// {print $1}' temp/list.csv | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9]{1,2}' | sort -u > result/blocked-ranges.txt
-
+sort -u tmp/include-ips.txt result/blocked-ranges.txt > result/blocked-ranges.txt
 
 # Generate OpenVPN route file
 echo -n > result/openvpn-blocked-ranges.txt
@@ -54,39 +44,8 @@ done < result/blocked-ranges.txt
 
 # Generate adguardhome aliases
 /bin/cp --update=none /root/adguardhome/* /opt/adguardhome/conf
-/bin/cp -f /opt/adguardhome/conf/upstream_dns_file_basis /opt/adguardhome/conf/upstream_dns_file
-while read -r line
-do
-    echo "[/$line/]127.0.0.4" >> /opt/adguardhome/conf/upstream_dns_file
-done < result/hostlist_zones.txt
-
-# # Generate dnsmasq aliases
-# echo -n > result/dnsmasq-aliases-alt.conf
-# while read -r line
-# do
-#     echo "server=/$line/127.0.0.4" >> result/dnsmasq-aliases-alt.conf
-# done < result/hostlist_zones.txt
-
-
-# # Generate knot-resolver aliases
-# echo 'blocked_hosts = {' > result/knot-aliases-alt.conf
-# while read -r line
-# do
-#     line="$line."
-#     echo "${line@Q}," >> result/knot-aliases-alt.conf
-# done < result/hostlist_zones.txt
-# echo '}' >> result/knot-aliases-alt.conf
-
-
-# # Generate squid zone file
-# echo -n > result/squid-whitelist-zones.conf
-# while read -r line
-# do
-#     echo ".$line" >> result/squid-whitelist-zones.conf
-# done < result/hostlist_zones.txt
-
-
-# Print results
-echo "Blocked domains: $(wc -l result/hostlist_zones.txt)" >&2
+/bin/cp -f /opt/adguardhome/conf/upstream_dns_file_basis result/adguard_upstream_dns_file
+sed -E -e 's~(.*)~[/\1/] 127.0.0.4~' result/hostlist_zones.txt > result/adguard_upstream_dns_file
+/bin/cp -f result/adguard_upstream_dns_file /opt/adguardhome/conf/upstream_dns_file
 
 exit 0
