07d28b38411539a47d2ad70e25ae28b1f47e9056
Alexander Pankratov <s.pankratoff@gmail.com>
2024-12-15 19:13:36 +0100

    Opt: remove temp file

--- rootfs/root/antizapret/update.sh
+++ rootfs/root/antizapret/update.sh
@@ -12,11 +12,11 @@ if [[ $SKIP_UPDATE_FROM_ZAPRET == true ]]; then
 fi
 
 LISTLINK='https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv.gz'
-curl -f --fail-early --compressed -o temp/list_orig.csv.gz "$LISTLINK" || exit 1
+curl -f --fail-early --compressed -o temp/list.csv.gz "$LISTLINK" || exit 1
 LISTSIZE="$(curl -sI "$LISTLINK"| awk 'BEGIN {IGNORECASE=1;} /content-length/ {sub(/[ \t\r\n]+$/, "", $2); print $2}')"
-[[ "$LISTSIZE" != "$(stat -c '%s' temp/list_orig.csv.gz)" ]] && echo "List 1 size differs" && exit 2
-gunzip -fd temp/list_orig.csv.gz || exit 3
-iconv -f cp1251 -t utf8 temp/list_orig.csv > temp/list.csv
+[[ "$LISTSIZE" != "$(stat -c '%s' temp/list.csv.gz)" ]] && echo "List 1 size differs" && exit 2
+gunzip -fd temp/list.csv.gz || exit 3
+iconv -f cp1251 -t utf8 temp/list.csv | sponge temp/list.csv
 
 
 NXDOMAINLINK='https://raw.githubusercontent.com/zapret-info/z-i/master/nxdomain.txt'
