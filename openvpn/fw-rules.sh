524f1d4ba7ff3994cd0ee797df606d935878af77
Alexander Pankratov <s.pankratoff@gmail.com>
2025-02-10 21:47:38 +0100

    Fix: discord subnets in openvpn

--- openvpn/fw-rules.sh
+++ openvpn/fw-rules.sh
@@ -9,17 +9,22 @@ OPENVPN_DNS='${OPENVPN_DNS:-"10.1.165.1"}'
 ANTIZAPRET_SUBNET=${ANTIZAPRET_SUBNET:-"10.224.0.0/15"}
 NIC='$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)' | head -1)'
 OVDIR='${OVDIR:-"/etc/openvpn"}'
+AZ_HOST='$(dig +short antizapret)'
 EOF
 source /etc/environment
 ln -sf /etc/environment /etc/profile.d/environment.sh
 
+iptables -t nat -N masq_not_local;
+iptables -t nat -A POSTROUTING -s ${OPENVPN_LOCAL_IP_RANGE}/24 -j masq_not_local;
+iptables -t nat -A masq_not_local -d ${AZ_HOST} -j RETURN;
+iptables -t nat -A masq_not_local -d ${ANTIZAPRET_SUBNET} -j RETURN;
+iptables -t nat -A masq_not_local -j MASQUERADE;
 
 touch $OVDIR/openvpn-blocked-ranges.txt
 if [ -f "/opt/antizapret/result/openvpn-blocked-ranges.txt" ]; then
     cp -f /opt/antizapret/result/openvpn-blocked-ranges.txt $OVDIR/openvpn-blocked-ranges.txt
 fi
 
-export AZ_HOST=$(dig +short antizapret)
 ip route add "$ANTIZAPRET_SUBNET" via "$AZ_HOST"
 
 if [[ ${FORCE_FORWARD_DNS:-true} == true ]]; then
