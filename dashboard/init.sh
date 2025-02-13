0860f77466bda96eaa38bf3d6c5bb34985b480db
cstantin <4959583+cstantin@users.noreply.github.com>
2025-02-13 15:53:34 +0300

    Feat: unified dashboard and self signed certificates for services

---
+++ dashboard/init.sh
@@ -0,0 +1,241 @@
+#!/usr/bin/env bash
+
+set -e
+
+CONFIG_DIR="/etc/nginx"
+CERT_DIR="$CONFIG_DIR/ssl"
+CERT_CRT="$CERT_DIR/selfsigned.crt"
+CERT_KEY="$CERT_DIR/selfsigned.key"
+SNIPPETS_DIR="$CONFIG_DIR/snippets"
+SSL_SNIPPET="$SNIPPETS_DIR/ssl-common.conf"
+PROXY_SNIPPET="$SNIPPETS_DIR/proxy-params.conf"
+SITES_AVAILABLE="$CONFIG_DIR/sites-available"
+SITES_ENABLED="$CONFIG_DIR/sites-enabled"
+SERVICES_JSON="$CONFIG_DIR/services.json"
+WWW_ROOT="/usr/share/nginx/html"
+
+
+declare -a services
+
+generate_certificate() {
+    echo "[INFO] Generating or checking SSL certificates..."
+
+    mkdir -p "$CERT_DIR"
+    if [ ! -f "$CERT_KEY" ] || [ ! -f "$CERT_CRT" ]; then
+        openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
+          -keyout "$CERT_KEY" \
+          -out "$CERT_CRT" \
+          -subj "/O=ANTIZAPRET/OU=ANTIZAPRET/CN=ANTIZAPRET"
+        echo "[INFO] Certificates have been generated."
+    else
+        echo "[INFO] Certificates already exist. Skipping generation."
+    fi
+    echo
+}
+
+create_snippets(){
+    echo "[INFO] Creating snippets configuration..."
+
+    mkdir -p "$SNIPPETS_DIR"
+    cat <<EOF | tee "$PROXY_SNIPPET" > /dev/null
+proxy_http_version 1.1;
+proxy_set_header Host $$host;
+proxy_set_header X-Real-IP $$remote_addr;
+proxy_set_header X-Forwarded-For $$proxy_add_x_forwarded_for;
+proxy_set_header X-Forwarded-Proto $$scheme;
+EOF
+
+    cat <<EOF | tee "$SSL_SNIPPET" > /dev/null
+server_name _;
+ssl_certificate     $CERT_CRT;
+ssl_certificate_key $CERT_KEY;
+ssl_protocols TLSv1.2 TLSv1.3;
+ssl_ciphers  HIGH:!aNULL:!MD5;
+EOF
+    echo "[INFO] Configuration files in $SNIPPETS_DIR have been created."
+}
+
+create_dashboard() {
+    echo "[INFO] Creating dashboard configuration..."
+
+    if [ -z "$DASHBOARD_USERNAME" ] || [ -z "$DASHBOARD_PASSWORD" ]; then
+      echo "[ERROR] DASHBOARD_USERNAME or DASHBOARD_PASSWORD environment variables are not set."
+      echo "Example usage:"
+      echo "  export DASHBOARD_USERNAME=user"
+      echo "  export DASHBOARD_PASSWORD=secret"
+      exit 1
+    fi
+
+    HTPASSWD_FILE="$CONFIG_DIR/.htpasswd"
+
+    HASHED_PASS="$(openssl passwd -apr1 "$DASHBOARD_PASSWORD")"
+    echo "$DASHBOARD_USERNAME:$HASHED_PASS" | tee "$HTPASSWD_FILE" > /dev/null
+    echo "[INFO] The file $HTPASSWD_FILE has been successfully created."
+
+    local conf_d_file="$CONFIG_DIR/conf.d/default.conf"
+    cat <<EOF | tee "$conf_d_file" > /dev/null
+server {
+    listen 1443 ssl;
+    server_name  _;
+
+    include $SSL_SNIPPET;
+
+    add_header "Access-Control-Allow-Origin" "*" always;
+    add_header "Access-Control-Allow-Methods" "*" always;
+    add_header "Access-Control-Allow-Headers" "*" always;
+
+    auth_basic           "Restricted Area";
+    auth_basic_user_file $HTPASSWD_FILE;
+
+    location / {
+        root   $WWW_ROOT;
+        index  index.html index.htm;
+    }
+
+    location /services.json {
+        alias $SERVICES_JSON;
+    }
+}
+EOF
+    echo "[INFO] The file $conf_d_file has been successfully created."
+    echo
+}
+
+read_services_env() {
+    local counter=1
+    while true; do
+        local varname="SERVICE_${counter}"
+        local value="${!varname}"
+        if [ -z "$value" ]; then
+            break
+        fi
+
+        value="${value//\"/}"
+        services+=("$value")
+        ((counter++))
+    done
+
+    if [ ${#services[@]} -eq 0 ]; then
+        echo "[ERROR] No variables of the type SERVICE_1, SERVICE_2, etc., were found."
+        exit 1
+    fi
+}
+
+create_services() {
+    echo "[INFO] Creating proxy configurations..."
+
+    mkdir -p "$SITES_AVAILABLE"
+    rm -rf "${SITES_AVAILABLE:?}"/*
+
+    echo "[INFO] Creating configuration files in $SITES_AVAILABLE..."
+    for service in "${services[@]}"; do
+        IFS=":" read -r name external_port internal_host internal_port <<< "$service"
+        conf_file="$SITES_AVAILABLE/${internal_host}.conf"
+        tee "$conf_file" > /dev/null <<EOF
+#${name}#
+server {
+    listen $external_port ssl;
+    include $SSL_SNIPPET;
+
+    location / {
+        include $PROXY_SNIPPET;
+        proxy_pass http://$internal_host:$internal_port;
+    }
+}
+EOF
+    done
+    echo "[INFO] Configuration files in $SITES_AVAILABLE have been created."
+    echo
+}
+
+enable_services() {
+    echo "[INFO] Enabling proxy configurations..."
+
+    mkdir -p "$SITES_ENABLED"
+    find "$SITES_ENABLED" -type l -exec rm -f {} \;
+
+    local conf_d_file="$CONFIG_DIR/conf.d/enabled-sites.conf"
+    echo "include $CONFIG_DIR/sites-enabled/*;" > $conf_d_file
+
+    echo "[INFO] Checking service availability..."
+
+    for conf_file in "$SITES_AVAILABLE"/*.conf; do
+        local backend_url host
+
+        backend_url=$(grep -E "proxy_pass\s+https?://" "$conf_file" 2>/dev/null \
+          | sed -E 's/.*proxy_pass\s+(https?:\/\/[^;]+);.*/\1/')
+        if [ -z "$backend_url" ]; then
+            echo "[INFO] No 'proxy_pass http/https' found in $conf_file — skipping."
+            continue
+        fi
+
+        host=$(echo "$backend_url" | sed -E 's|^https?://([^/]+)/?.*|\1|' | cut -d: -f1)
+
+        if getent hosts "$host" >/dev/null; then
+            ln -sf "$conf_file" "$SITES_ENABLED/$(basename "$conf_file")"
+            echo "[OK] $host is available — enabling configuration $(basename "$conf_file")."
+        else
+            echo "[WARN] $host is unavailable — skipping symlink creation."
+        fi
+    done
+    echo
+}
+
+export_services() {
+    echo "[INFO] Configuring services in dashboard ..."
+
+    rm -f "$SERVICES_JSON"
+
+    echo "[" > "$SERVICES_JSON"
+
+    local index=1
+
+    for service in "${services[@]}"; do
+        IFS=":" read -r name external_port internal_host internal_port <<< "$service"
+
+        local conf_file="$SITES_ENABLED/${internal_host}.conf"
+        if [ ! -f "$conf_file" ]; then
+            echo "[WARN] $conf_file not found, skipping..."
+            continue
+        fi
+
+        local backend_url enabled_name enabled_port enabled_host
+
+        backend_url=$(grep -E "proxy_pass\s+https?://" "$conf_file" 2>/dev/null \
+          | sed -E 's/.*proxy_pass\s+(https?:\/\/[^;]+);.*/\1/')
+        if [ -z "$backend_url" ]; then
+            echo "[INFO] No 'proxy_pass http/https' found in $conf_file — skipping."
+            continue
+        fi
+
+        enabled_host=$(echo "$backend_url" | sed -E 's|^https?://([^/]+)/?.*|\1|' | cut -d: -f1)
+        enabled_port="$(grep -m1 'listen' "$conf_file" | sed 's/^.*listen \([0-9]\+\) ssl.*$/\1/')"
+        enabled_name="$(grep -m1 '^#' "$conf_file" | sed 's/^#\([^#]*\)#$/\1/')"
+
+        [ -z "$enabled_host" ] && enabled_name="$internal_host"
+        [ -z "$enabled_name" ] && enabled_name="$name"
+        [ -z "$enabled_port" ] && enabled_port="$external_port"
+
+        cat <<EOF >>"$SERVICES_JSON"
+  {
+    "name": "$enabled_name",
+    "hash": "$enabled_host",
+    "port": $enabled_port
+  },
+EOF
+
+        ((index++))
+    done
+    sed -i -e '1h;1!H;$!d;x;s/\(.*\),/\1/' $SERVICES_JSON
+    echo "]" >> "$SERVICES_JSON"
+    echo "[INFO] $SERVICES_JSON created."
+}
+
+generate_certificate
+create_snippets
+create_dashboard
+read_services_env
+create_services
+enable_services
+export_services
+echo "[INFO] All steps completed."
