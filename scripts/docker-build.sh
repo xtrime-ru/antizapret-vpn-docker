d63183862fd489fb6981d941d6e1cce4c6a29c81
Alexander Pankratov <s.pankratoff@gmail.com>
2025-02-16 00:13:12 +0100

    Remove container names and images

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd .. && pwd )";
 
-exec docker buildx build --platform linux/amd64,linux/arm64 -t xtrime/antizapret-core:latest "$@" --push "$SCRIPT_DIR"
+exec docker buildx build --platform linux/amd64,linux/arm64 -t xtrime/antizapret-vpn:latest "$@" --push "$SCRIPT_DIR"
