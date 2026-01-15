59bdc2890f3979fd5a1e6ef5340dd69bbe9cf35f
Alexander Shkarlatov <crazyalex25@mail.ru>
2026-01-15 15:34:45 +0300

    fix: filebrowser. 500 internal error. Permission denied

--- services/filebrowser/hooks/doall.sh
+++ services/filebrowser/hooks/doall.sh
@@ -4,6 +4,6 @@ STAGE_1=false
 STAGE_2=true
 STAGE_3=true
 
-curl -X POST http://antizapret:8080/doall \
+curl -X POST http://az-local.antizapret:80/doall \
      -H "Content-Type: application/json" \
      -d '{ "stage_1": '"$STAGE_1"', "stage_2": '"$STAGE_2"', "stage_3": '"$STAGE_3"' }'
