c09b8bfaede909a5eeb729d4b41513b968f613e5
Даниил Селиверстов <46894057+danayer@users.noreply.github.com>
2026-03-03 13:21:31 +0800

    Add POST request to az-world endpoint

--- services/filebrowser/hooks/doall.sh
+++ services/filebrowser/hooks/doall.sh
@@ -7,3 +7,7 @@ STAGE_3=true
 curl -X POST http://az-local.antizapret:80/doall \
      -H "Content-Type: application/json" \
      -d '{ "stage_1": '"$STAGE_1"', "stage_2": '"$STAGE_2"', "stage_3": '"$STAGE_3"' }'
+
+curl -X POST http://az-world.antizapret:80/doall \
+     -H "Content-Type: application/json" \
+     -d '{ "stage_1": '"$STAGE_1"', "stage_2": '"$STAGE_2"', "stage_3": '"$STAGE_3"' }'
